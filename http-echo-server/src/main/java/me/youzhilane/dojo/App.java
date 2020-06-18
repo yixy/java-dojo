b4cf1f6c5ecf6a5b64a205d650c7f874541c1629
yixy <youzhilane01@gmail.com>
2020-06-18 18:50:22 +0800

    mod pom
    Signed-off-by: yixy <youzhilane01@gmail.com>

---
+++ http-echo-server/src/main/java/me/youzhilane/dojo/App.java
@@ -0,0 +1,22 @@
+package me.youzhilane.dojo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class App {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
